139f10fbce2904bc922ceedab958ec3dc97edaaa
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-04-04 18:20:16 -0300

    Starting class

---
+++ includes/TourRepairer.hpp
@@ -0,0 +1,10 @@
+#ifndef TOURREPAIRER_HPP
+#define TOURREPAIRER_HPP
+class TourRepairer{
+    //Step 1- Find the overloaded tours
+    //Step 2- Find the customer whom demands the most
+    //Step 3- Move this customer to the tour with less load
+    //Step 4- Test inside the tour to check which position will impact less on the overral distance
+    // Repeat until there is not any overloaded tours
+};
+#endif 
--- sources/main.cpp
+++ sources/main.cpp
@@ -19,7 +19,7 @@ void debugGA();
 
 void setParams(){
     //ImportData file("libs/more-libs/E-n33-k4.vrp");
-    ImportData file("libs/P-n22-k2.vrp");
+    ImportData file("libs/P-Sets/P-n22-k2.vrp");
     Configs::truckNumber=5;
     Configs::customerMap=CustomerMap(file.getCustomerList(),file.getCapacity(),Configs::truckNumber)    ;
     Configs::InitialPopmutIterations=100;
@@ -34,7 +34,7 @@ int main(){
     int start_s=clock();
 
     //startGA();
-    debugGA();
+    //debugGA();
 
     int stop_s=clock();
     cout << "time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000<< endl;
